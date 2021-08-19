3b46007832f533055429523ca0b3f5c10d7c44b5
Benjamin Poon <benjamintpoon@gmail.com>
2021-08-19 10:40:05 -0500

    parcel

--- adopt-me/src/app.js
+++ adopt-me/src/app.js
@@ -1,18 +1,12 @@
-/* global React ReactDOM */
-
-const Pet = (props) => {
-  return React.createElement("div", {}, [
-    React.createElement("h2", {}, props.name),
-    React.createElement("h3", {}, props.animal),
-    React.createElement("h3", {}, props.breed),
-  ]);
-};
+import React from "react";
+import ReactDOM from "react-dom";
+import Pet from "./Pet";
 
 const App = () => {
   return React.createElement("div", {}, [
-    React.createElement("h1", {}, "Adopt Me"),
+    React.createElement("h1", { id: "my-brand" }, "Adopt Me"),
     React.createElement(Pet, {
-      name: "Toby",
+      name: "TobyBoy",
       animal: "Dog",
       breed: "Pit Bull/Shepherd",
     }),
