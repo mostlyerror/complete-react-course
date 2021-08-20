6ae2d247dbd836e2148492517d18bca9d4913772
Benjamin Poon <benjamintpoon@gmail.com>
2021-08-19 20:18:02 -0500

    search params, hooks

--- adopt-me/src/app.js
+++ adopt-me/src/app.js
@@ -1,14 +1,11 @@
 import ReactDOM from "react-dom";
-import Pet from "./Pet";
+import SearchParams from "./SearchParams";
 
 const App = () => {
   return (
     <div>
       <h1>Adopt Me</h1>
-      <Pet name="TobyBoy" animal="dog" breed="Pit Bull/Shepherd" />
-      <Pet name="Lola" animal="dog" breed="Lab/Shepherd" />
-      <Pet name="Worm" animal="dog" breed="Heeler" />
-      <Pet name="BearBear" animal="dog" breed="Corgi" />
+      <SearchParams />
     </div>
   );
 };
